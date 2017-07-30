9f320c35c325b16a97cb336f6e5342380368295b
99Wolves <99wolves17@gmail.com>
2017-07-30 13:53:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ client.on('guildCreate', guild => {
 client.on('guildMemberAdd',member => {
   let guild = member.guild;
   guild.defaultChannel.sendMessage('Please welcome ${member.user.username} to the server!')
-})
+});
 
 client.on('guildMemberRemove' , member => {
   let guild = member.guild;
@@ -33,11 +33,27 @@ var prefix = "/"
 client.on('message', message => {
   let args = message.content.split('').slice(1);
   var result = args.join('');
-  member => [ here ] > ) < [here] { 
+
 });
 if(!message.content.startWith(prefix)) return;
 if(message.author.bot) return;
 }
 
- 
+if(message.content.startsWith(prefix + 'ping')) {
+  message.channel.sendMessage('Pong!'\#{Date.now() - message.createdTimestamp} ms\'');
+}else
+
+  if(message.content.startsWith(prefix + 'send')) {
+    client.channels.get('253305415230554113').sendMessage('Hello to Server!');
+
+  }else
+if(message.content.startsWith(prefix + 'setgame')){
+  if(!result){
+    result = null
+  }
+  client.user.setGame(result); 
+}
+}
+}
+
 client.login('MzQwMzQ5MzI4MTM0NjM1NTIx.DFxP2g.aQznpqb-zYYSRb7pwXiVKiQ18bs');
